7201af6719b4f1e20891c0141c4b7984c99e2094
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2025-04-03 16:48:50 -0600

    feat: login functionality

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -41,4 +41,31 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password }  = req.body;
+        if(!email || !password ) return res.status(400).json({message:"You need to provide a password and email"})
+        const db = await connectToDatabase();
+        const collection = db.collection("users")
+        const userExist = await collection.findOne({email: email});
+        if(!userExist) return res.status(400).json({message: "This email is not registered"})
+        const result = await bcrypt.compare(password, userExist.password);
+        
+        if(!result) return res.status(401).json({message:"Invalid credentials"})
+
+        const payload = {
+            user: {
+                id: userExist._id.toString()
+            }
+        }
+        const authtoken = jwt.sign(payload, JWT_SECRET)
+        ;
+        logger.info('User logged successfully');
+        res.status(200).json({authtoken, usernName: userExist.firstName, userEmail: userExist.email})
+        
+    } catch (error) {
+       next(error); 
+    }
+});
+
 module.exports = router;
